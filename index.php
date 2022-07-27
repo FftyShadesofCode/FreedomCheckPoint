c43e8624aeed7d8b3b37508b87087bba14d44259
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-07-27 01:09:19 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Freedom Point</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/css/bootstrap.min.css" integrity="sha512-XWTTruHZEYJsxV3W/lSXG1n3Q39YIWOstqvmFsdNEEQfHoZ6vm6E9GK2OrF6DSJSpIbRbi+Nn0WDPID9O7xB2Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Bodoni:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Uchen&display=swap" rel="stylesheet">
+
+    <style>
+        * {
+            font-family: 'Libre Bodoni', serif;
+        }
+        .h-font {
+            font-family: 'Uchen', serif;
+        }
+    </style>
+</head>
+
+<body>
+
+<!--Nav-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-evenly">
+    <a class="navbar-brand" href="#">Freedom CheckPoint</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Check Ins</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Projections</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">728</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Daily Lodging</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">729</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sandwich</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Breakdown</a>
+            </li>
+        </ul>
+        <form class="d-flex">
+            <input type="search" class="form-control me-2" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success" type="submit" >Search</button>
+        </form>
+    </div>
+</nav>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.bundle.min.js" integrity="sha512-9GacT4119eY3AcosfWtHMsT5JyZudrexyEVzTBWV3viP/YfB9e2pEy3N7WXL3SV6ASXpTU0vzzSxsbfsuUH4sQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
