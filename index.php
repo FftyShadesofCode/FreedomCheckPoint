986859207210f061a29c9932ec512d584809298a
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-07-27 20:20:48 -0400

    Added in images, and a few modals for login and registration

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.9.1/font/bootstrap-icons.min.css" integrity="sha512-5PV92qsds/16vyYIJo3T/As4m2d8b6oWYfoqV+vtizRB6KhF1F9kYzWzQmsO6T3z3QG2Xdhrx7FQ+5R1LiQdUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.3.2/swiper-bundle.css" integrity="sha512-ipO1yoQyZS3BeIuv2A8C5AwQChWt2Pi4KU3nUvXxc4TKr8QgG8dPexPAj2JGsJD6yelwKa4c7Y2he9TTkPM4Dg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Libre+Bodoni:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Uchen&display=swap" rel="stylesheet">
@@ -20,6 +22,18 @@
         }
         .h-font {
             font-family: 'Uchen', serif;
+        }
+
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+
+        /* Firefox */
+        input[type=number] {
+            -moz-appearance: textfield;
         }
     </style>
 </head>
@@ -57,14 +71,15 @@
             </li>
         </ul>
         <form class="d-flex">
-            <button class="btn btn-outline-success shadow-none" data-bs-toggle="modal" data-bs-target="#loginModal" type="button" >Login</button>
+            <button class="btn btn-outline-success shadow-none me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#loginModal" type="button" >Login</button>
+            <button class="btn btn-outline-success shadow-none me-lg-2 me-3" data-bs-toggle="modal" data-bs-target="#registerModal" type="button" >Register</button>
         </form>
     </div>
 </nav>
 
 <!-- Modal -->
 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog">
         <div class="modal-content">
             <form>
                 <div class="modal-header">
@@ -76,17 +91,93 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-outline-success">Save changes</button>
+                    <div class="mb-3">
+                        <label for="form-label">Email Address</label>
+                        <input type="email" class="form-control shadow-none" placeholder="Enter email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control shadow-none" placeholder="Password">
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between mb-2">
+                        <button type="button" class="btn btn-outline-success shadow-none">Login</button>
+                        <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+                    </div>
                 </div>
             </form>
         </div>
     </div>
 </div>
 
+<div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <form>
+                <div class="modal-header">
+                    <h5 class="modal-title d-flex align-items-center">
+                        <i class="bi bi-person fs-3 me-2"></i>User Registration
+                    </h5>
+                    <button type="reset" class="btn-close shadow-none" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="form-label">Email Address</label>
+                        <input type="email" class="form-control shadow-none" placeholder="Enter email">
+
+                        <label for="form-label">Confirm Email Address</label>
+                        <input type="email" class="form-control shadow-none" placeholder="Re-Enter email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control shadow-none" placeholder="Password">
+
+                        <label for="exampleInputPassword1">Confirm Password</label>
+                        <input type="password" class="form-control shadow-none" placeholder="Confirm Password">
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between mb-2">
+                        <button type="button" class="btn btn-outline-success shadow-none">Register</button>
+                        <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <!-- Swiper -->
+    <div class="swiper swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="./images/lounge/IMG_6625.jpeg" alt="" class="w-100 d-block">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/lounge/IMG_6626.jpeg" alt="" class="w-100 d-block">
+            </div>
+            <div class="swiper-slide">
+                <img src="./images/lounge/IMG_6627.jpeg" alt="" class="w-100 d-block">
+            </div>
+        </div>
+    </div>
+</div>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.bundle.min.js" integrity="sha512-9GacT4119eY3AcosfWtHMsT5JyZudrexyEVzTBWV3viP/YfB9e2pEy3N7WXL3SV6ASXpTU0vzzSxsbfsuUH4sQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.3.2/swiper-bundle.min.js" integrity="sha512-V1mUBtsuFY9SNr+ptlCQAlPkhsH0RGLcazvOCFt415od2Bf9/YkdjXxZCdhrP/TVYsPeAWuHa+KYLbjNbeEnWg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script>
+    const swiper = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true,
+        autoPlay: {
+            delay: 3500,
+            disableOnInteraction: false,
+        }
+    });
+</script>
+
 </body>
 </html>
